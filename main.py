26a6505e90eee6525b96c1b2d3f77fbc3041bf03
atseven <48451542+atseven@users.noreply.github.com>
2022-02-15 16:10:51 +0500

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-from fastapi import FastAPI
+from flask import Flask
+app = Flask(__name__)
 
-@app.get('/forecast')
-def forecast(name: str):
-    return {'message': name}
+@app.get('/')
+def forecast():
+    return {'message': "rana"}
+
+if __name__ == "__main__":
+    app.run()
