1fd8c2df5919f2dcb7dc94174d716f49b1bb55c1
atseven <48451542+atseven@users.noreply.github.com>
2022-02-16 14:26:04 +0500

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,9 @@
 from flask import Flask
 app = Flask(__name__)
 
-@app.get('/')
-def forecast():
-    return {'message': "rana"}
+@app.route('/')
+def forecast(name: str, time: int):
+    return {'message': name, 'time': time}
 
 if __name__ == "__main__":
     app.run()
