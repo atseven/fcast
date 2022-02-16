45ca5a5106e4eba989f1d0a7ab272400226c762a
atseven <48451542+atseven@users.noreply.github.com>
2022-02-16 16:02:43 +0500

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 @app.route('/<path:path>')
 def forecast(path):
 #     path = request.args.get('filePath')
-    return send_file(path, as_attachment=True)
+    return send_file(path[5:], as_attachment=True)
 #     return {'name': request.args.get('name'), 'time': request.args.get('time')}
 
 if __name__ == "__main__":
